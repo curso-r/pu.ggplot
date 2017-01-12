e2094e343459c9ee816eb7ffb0bc3d42a36357f3
Daniel Falbel <dfalbel@gmail.com>
2017-01-11 22:50:28 -0200

    fix deploy sh

--- _deploy.sh
+++ _deploy.sh
@@ -5,8 +5,8 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "abj@abjur.org.br"
-git config --global user.name "ABJ"
+git config --global user.email "dfalbel@gmail.com"
+git config --global user.name "Daniel Falbel"
 
 git clone -b content https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git out
 cd out
